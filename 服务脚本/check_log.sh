0645e5d4c6e85f52c374c146c14d10f23b5b1132
Administrator <admin@example.com>
2019-02-15 10:46:03 +0800

    update

--- 服务脚本/check_log.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#用途：删除大于7天的日志文件
-
-
-find /www/fanda_new-agent/runtime/Logs/Home/ -mtime +7 -name "*.log" -exec rm -rf {} \;
-find /www/fanda_new-agent/runtime/Logs/Api/ -mtime +7 -name "*.log" -exec rm -rf {} \;
-find /www/fanda_new-agent/runtime/Logs/Admin/ -mtime +7 -name "*.log" -exec rm -rf {} \;
